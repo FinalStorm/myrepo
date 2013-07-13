d0b0098becc2d66c095ae86ae1710bfbc4da1638
FinalStorm <info@czechsite.net>
2013-07-13 11:55:32 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,33 +1,14 @@
-var fs = require('fs');
-var outfile = "hello.txt";
-var out = "";
-var n = 0  
-var i = 1
+var express = require('express');
 
-while (n  < 101)
-{ 
-	var delitelne = false;
-	var k=2;
-	while (i>k)
-	{ 
-		var pod = i/k;
-		var fl = Math.floor(pod);
-		if(fl==pod)	
-		{
-		 delitelne = true;	
-		}
-	 k++		
-	}	
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response){
+	response.send('Hello honza!');
+});
 	
-	if(delitelne == false)
-	{
-		out = out + i + ",";
-		n++
-	}	
-	i++
-}
 
+var port= process.env.PORT ||5000;
 
-fs.writeFileSync(outfile, out);  
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);  
-
+app.listen(port, function(){
+	console.log("Listening on " + port);
+});
