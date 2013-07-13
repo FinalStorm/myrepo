61b027214aa26c1ff6ae69d5be2d6795b9eb2559
FinalStorm <info@czechsite.net>
2013-07-13 12:32:58 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,12 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 
-
-
-
 	
 var data = fs.readFileSync('index.html');
-
+var buf = new Buffer(256);
+buf.toString("utf-8")
 
 
 app.get('/', function(request, response){
