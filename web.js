f5b1f04777b26ee37d18b65f814651344742a89c
FinalStorm <info@czechsite.net>
2013-07-13 12:27:45 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+
+
+
+	
+var data = fs.readFileSync('index.html');
+
+
 
 app.get('/', function(request, response){
-	response.send('Hello honza!');
+		response.send(data);
 });
-	
-
 var port= process.env.PORT ||5000;
 
 app.listen(port, function(){
